2bb7311aae3a918b28b032d7f9d55418d0a80a9d
Nicolas Powell <nicopowell30@gmail.com>
2025-05-20 12:40:50 -0300

    se termino el ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string texto = Console.ReadLine();
+int numero;
+bool esNum = int.TryParse(texto, out numero);
+
+if (esNum)
+{
+    if (numero > 0)
+    {
+        int invertido = 0;
+        while (numero > 0)
+        {
+            invertido = invertido * 10 + numero % 10;
+            numero /= 10;
+        }
+        Console.WriteLine("El numero invertido es " + invertido);
+    }
+    else
+    {
+        Console.WriteLine("El numero ingresado no es mayor que cero");
+    }
+}
+else
+{
+    Console.WriteLine("El texto ingresado no es un numero entero");
+}
