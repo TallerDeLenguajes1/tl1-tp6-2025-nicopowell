d1864a6e33645876b0acd553ae1d6991fc72662a
Nicolas Powell <nicopowell30@gmail.com>
2025-05-27 10:03:18 -0300

    se creo la calculadora del ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// EJERCICIO 1
+
+/*
 string texto = Console.ReadLine();
 int numero;
 bool esNum = int.TryParse(texto, out numero);
@@ -24,4 +27,61 @@ if (esNum)
 else
 {
     Console.WriteLine("El texto ingresado no es un numero entero");
+}
+*/
+
+// EJERCICIO 2 Y 3
+
+using System.Runtime.CompilerServices;
+
+float solicitarNumero()
+{
+    string numString = Console.ReadLine();
+    float num;
+
+    while (!float.TryParse(numString, out num))
+    {
+        Console.Write("Ingrese un numero valido");
+        numString = Console.ReadLine();
+    }
+    return num;
+}
+
+Console.Write("Ingrese un numero: ");
+float num = solicitarNumero();
+
+Console.WriteLine("|" + num + "| = " + Math.Abs(num));
+Console.WriteLine(num + "^2 = " + num * num);
+if (num >= 0)
+{
+    Console.WriteLine("Raiz cuadrada de " + num + " = " + Math.Sqrt(num));
+}
+else
+{
+   Console.WriteLine("No podemos calcular raiz cuadrada de un numero negativo"); 
+}
+Console.WriteLine("sen(" + num + ") = " + Math.Sin(num));
+Console.WriteLine("cos(" + num + ") = " + Math.Cos(num));
+Console.WriteLine("Parte entera de " + num + " = " + Math.Truncate(num));
+
+Console.WriteLine("=====================");
+
+Console.Write("Ingrese el numero 1: ");
+float num1 = solicitarNumero();
+Console.Write("Ingrese el numero 2: ");
+float num2 = solicitarNumero();
+
+if (num1 > num2)
+{
+    Console.WriteLine("Maximo: " + num1);
+    Console.WriteLine("Minimo: " + num2);
+}
+else if (num2 > num1)
+{
+    Console.WriteLine("Maximo: " + num2);
+    Console.WriteLine("Minimo: " + num1);
+}
+else
+{
+    Console.WriteLine("Los dos numeros son iguales");
 }
